cf83b92e97d6a5d1ef6443f49fde5c9c763c55c5
Tobias Möritz <tobias@moeritz.cc>
2023-06-28 23:27:38 +0200

    add linkType & linkTitle functions & update to new Kirby v4 alpha

--- config/fieldMethods.php
+++ config/fieldMethods.php
@@ -5,9 +5,13 @@ use Kirby\Toolkit\Str;
 
 return [
   // based on:
-  // https://github.com/getkirby/kirby/blob/v4/develop/panel/src/components/Forms/Field/LinkField.vue#L185
-  'urlType' => function (Field $field) {
+  // https://github.com/getkirby/kirby/blob/v4/develop/panel/src/components/Forms/Field/LinkField.vue#L127
+  'linkType' => function (Field $field) {
     $val = $field->value();
+
+    if (Str::match($val, '/^(http|https):\/\//')) {
+      return 'url';
+    }
 
     if (Str::startsWith($val, 'page://') || Str::startsWith($val, '/@/page/')) {
       return 'page';
@@ -25,6 +29,35 @@ return [
       return 'email';
     }
 
-    return 'url';
+    if (Str::startsWith($val, '#')) {
+      return 'anchor';
+    }
+
+    return 'custom';
+  },
+  'urlType' => function (Field $field) {
+    return $field->linkType();
+  },
+  'linkTitle' => function (Field $field) {
+    $val = $field->value();
+    $type = $field->linkType();
+
+    switch ($type) {
+      case 'url':
+        $url = parse_url($val);
+        return $url['hostname'] . ($url['path'] ?? "");
+      case 'page':
+        $page = $field->toPage();
+        if ($page) return $page->title();
+      case 'file':
+        $file = $field->toFile();
+        if ($file) return $file->filename();
+      case 'email':
+        return Str::replace($val, 'mailto:', '');
+      case 'tel':
+        return Str::replace($val, 'tel:', '');
+      default:
+        return $val;
+    };
   }
 ];
